47b0ce59f41995be5e88b99647852e521a25b254
Zamzay <93517658+Zamzay@users.noreply.github.com>
2026-05-10 18:15:20 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -8,55 +8,54 @@ struct SYM {
 };
 
 template<typename T>
-class TPQueue {
 private:
- struct Item {
- T data;
- Item* next;
- };
- Item* head;
+  struct Item {
+    T data;
+    Item *next;
+  };
+
+  Item *head = nullptr;
+
+  Item *create(const T &value) {
+    Item *item = new Item;
+    item->data = value;
+    item->next = nullptr;
+    return item;
+  }
 
 public:
- TPQueue() : head(nullptr) {}
+  void push(const T &value) {
+    Item *item = create(value);
 
- void push(const T& value) {
-   Item *item = new Item<T>(value)
-   if (head == nullptr) {
-     head = item;
-     return;
-   } else if (value.prior > head->data.prior) {
-     item->next = head;
-     head = item;
-     return;
-   } else {
-     Item* curItem = head;
-     while (curItem->next && curItem->next->data.prior >= value.prior) {
-       curItem = curItem->next;
-     }
-     item->next = curItem->next;
-     curItem->next = item;
-   }
- }
+    if (head == nullptr) {
+      head = item;
+      return;
+    }
+    if (value.prior > head->data.prior) {
+      item->next = head;
+      head = item;
+      return;
+    }
 
- T pop() {
-   if (!head) {
-     throw std::runtime_error("Queue is empty");
-   }
-   
-   Item* temp = head;
-   T value = temp->data;
-   head = head->next;
-   delete temp;
-   return value;
- }
+    Item *curItem = head;
+    while (curItem->next && curItem->next->data.prior >= value.prior) {
+      curItem = curItem->next;
+    }
+    item->next = curItem->next;
+    curItem->next = item;
+  }
 
- ~TPQueue() {
-   while (head != nullptr) {
-     Item<T>* temp = head;
-     head = head->next;
-     delete temp;
-   }
- }
+  T pop() {
+    if (!head) {
+      throw std::runtime_error("Queue is empty");
+    }
+
+    Item *temp = head;
+    T value = temp->data;
+    head = head->next;
+    delete temp;
+
+    return value;
+  }
 };
-
 #endif  // INCLUDE_TPQUEUE_H_
